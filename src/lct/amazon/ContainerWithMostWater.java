41f58445d2cade18e154f14620dd129885099677
edivagner@gmail.com <Bqsqddtasf2020?>
2022-10-07 15:58:13 +0100

    ContainerWithMostWater

--- src/lct/amazon/ContainerWithMostWater.java
+++ src/lct/amazon/ContainerWithMostWater.java
@@ -2,10 +2,43 @@ package lct.amazon;
 
 public class ContainerWithMostWater {
 
+	public int maxArea(int[] height) {
+		int maxarea = 0;
+		for (int left = 0; left < height.length; left++) {
+			for (int right = left + 1; right < height.length; right++) {
+				int width = right - left;
+				maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
+			}
+		}
+		return maxarea;
+	}
+	
+	public int maxArea2(int[] height) {
+        int maxarea = 0;
+        int left = 0; 
+        int right = height.length - 1;
+        while (left < right) {
+            int width = right - left;
+            maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * width);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxarea;
+    }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hi");
-	}
+		ContainerWithMostWater cwmw = new ContainerWithMostWater();
 
+		int[] nums = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+		System.out.println(cwmw.maxArea(nums));
+		
+		System.out.println(cwmw.maxArea2(nums));
+
+	}
 }
